335254a4d1ccb0b7d1775899b2520d03d6aa15da
railskipl <rails@kunalinfotech.net>
2012-06-19 14:10:48 +0530

    head

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 # before_filter :authenticate
 
  # before_filter :correct_user, :only => [:edit, :update]
-=======
+
     before_filter  :authenticate ,:only => [:edit,:update]
     before_filter :correct_user,:only => [:edit,:update,:destroy]
 
